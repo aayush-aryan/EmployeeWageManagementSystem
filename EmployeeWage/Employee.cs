e1552c251941b1622d46d0647ac38446d7451cd6
aayush-aryan <log2aayusharyan@gmail.com>
2022-07-14 17:37:26 +0530

    added. method for calculating monthlyWages

--- EmployeeWage/Employee.cs
+++ EmployeeWage/Employee.cs
@@ -9,11 +9,12 @@ namespace EmployeeWage
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
 
         public static int empCheck;
-        public static int empHrs;
-        public static int empWage;
-
+        public static int empHrs = 0;
+        public static int empWage = 0;
+        public static int totalEmpWages = 0;
 
         public static int RandomIntegerGenerator(int minRange, int maxRange)
         {
@@ -22,29 +23,30 @@ namespace EmployeeWage
             return empCheck;
         }
 
-        public static void AddingPartTimeEmployeeWagesUsingSwitchCase()
+        public static void CalculatingMonthlyWages()
         {
-            
-            empCheck = Employee.RandomIntegerGenerator(0,3);
-            switch (empCheck)
+
+            for (int day = 1; day <= NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    Console.WriteLine("Employee is Full Time");
-                    break;
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    Console.WriteLine("Employee is Part Time");
-                    break;
-                default:
-                    empHrs = 0;
-                    Console.WriteLine("Employee is absent");
-                    break;
+                empCheck = Employee.RandomIntegerGenerator(0, 3);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWages += empWage;
+                Console.WriteLine("Employee Daily Wages day" + day + ":" + empWage);
             }
-
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee Daily Wages :" + empWage);
+            Console.WriteLine("Employee Monthly Wages :" + totalEmpWages);
         }
-    }
 
+    }
 }
