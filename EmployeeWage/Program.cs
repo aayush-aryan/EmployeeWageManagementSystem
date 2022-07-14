4d82a7409ac1c0dc1d01ade5a401fe465ccb4329
aayush-aryan <log2aayusharyan@gmail.com>
2022-07-14 14:48:26 +0530

    added welcome message for EmployeeWage

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To EmployeeWage Management System ");
+
+           
+        }
+    }
+}
