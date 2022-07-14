27e467098067f82ba2418c3b74019ca161406264
aayush-aryan <log2aayusharyan@gmail.com>
2022-07-14 15:16:01 +0530

    added method for checking EmployeePresent Or Not

--- EmployeeWage/Employee.cs
+++ EmployeeWage/Employee.cs
@@ -6,9 +6,28 @@ namespace EmployeeWage
 {
     class Employee
     {
-      
+        public const int IS_FULL_TIME = 1;
+        public static int empCheck;
+        public static int RandomIntegerGenerator(int minRange, int maxRange)
+        {
+            Random random = new Random();
+            empCheck = random.Next(minRange, maxRange);
+            return empCheck;
+        }
 
-
+        public static void AttendanceCheck()
+        {
+            empCheck = Employee.RandomIntegerGenerator(0, 2);
+            Console.WriteLine(empCheck);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
     }
 
 }
